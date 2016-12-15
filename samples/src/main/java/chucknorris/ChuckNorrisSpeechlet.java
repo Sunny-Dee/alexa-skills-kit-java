21abaa1c1f2531d87194dc47888111cfecae14f4
Deliana Escobari <delianaescobari@gmail.com>
2016-12-15 12:30:27 -0600

    All code functions coded. Untested

--- samples/src/main/java/chucknorris/ChuckNorrisSpeechlet.java
+++ samples/src/main/java/chucknorris/ChuckNorrisSpeechlet.java
@@ -1,9 +1,17 @@
 package chucknorris;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.amazon.speech.slu.Intent;
 import com.amazon.speech.speechlet.IntentRequest;
 import com.amazon.speech.speechlet.LaunchRequest;
 import com.amazon.speech.speechlet.Session;
@@ -14,6 +22,8 @@ import com.amazon.speech.speechlet.SpeechletException;
 import com.amazon.speech.speechlet.SpeechletResponse;
 import com.amazon.speech.ui.Reprompt;
 import com.amazon.speech.ui.SsmlOutputSpeech;
+import com.amazonaws.util.json.JSONException;
+import com.amazonaws.util.json.JSONObject;
 
 public class ChuckNorrisSpeechlet implements Speechlet {
 	private static final Logger log = LoggerFactory.getLogger(ChuckNorrisSpeechlet.class);
@@ -33,14 +43,72 @@ public class ChuckNorrisSpeechlet implements Speechlet {
 
 	@Override
 	public SpeechletResponse onIntent(IntentRequest request, Session session) throws SpeechletException {
-		// TODO Auto-generated method stub
-		return null;
+		log.info("onIntent RequestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
+		
+		Intent intent = request.getIntent();
+		String intentName = intent.getName();
+		
+		if ("HitMe".equals(intentName)){
+			return getJoke();
+		} else if ("AMAZON.HelpIntent".equals(intentName)) {
+			String help = "I give you an ass kicking joke if you say... ask Chuck Norris to give me a joke";
+			return generateAskResponse(help, help);
+		} else {
+			throw new SpeechletException("Invalid intent");
+		}
 	}
 
 	@Override
 	public void onSessionEnded(SessionEndedRequest request, Session session) throws SpeechletException {
 		log.info("onSessionEnded ReauestId={}, SessionId{}", session.getSessionId(), request.getRequestId());
 		
+	}
+	
+	private SpeechletResponse getJoke() {
+		String joke = parseJson(makeAPIRequest());
+		String reprompt ="Don't be shy ask for another joke";
+		return generateAskResponse(joke, reprompt);
+	}
+	
+	private String makeAPIRequest(){
+		InputStreamReader inputStream = null;
+		BufferedReader bufferedReader = null;
+		String text = "";
+		
+		try {
+			String line;
+			URL url = new URL(URL_PREFIX);
+			inputStream = new InputStreamReader(url.openStream(), Charset.forName("US-ASCII"));
+			bufferedReader = new BufferedReader(inputStream);
+			StringBuilder builder = new StringBuilder();
+			
+			while ((line = bufferedReader.readLine()) != null){
+				builder.append(line);
+			}
+			text = builder.toString();
+		} catch (IOException e){
+			e.printStackTrace();
+			text = "";
+		} finally {
+			IOUtils.closeQuietly(inputStream);
+			IOUtils.closeQuietly(bufferedReader);
+		}
+		
+		return text;
+	}
+	
+	private String parseJson(String text){
+		String joke = "";
+		try {
+			JSONObject response = new JSONObject(text);
+			JSONObject value = new JSONObject(response.getJSONObject("value"));
+			joke = value.getString("joke");
+		} catch (JSONException e){
+			joke = "No jokes for you, just a kick on the valuables.";
+			e.printStackTrace();
+		}
+		
+		return joke;
 	}
 	
 	private SpeechletResponse getWelcomeResponse() {
