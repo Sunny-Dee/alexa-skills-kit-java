276cd64121e9a8cf118e0e644a55f3d4765592c1
Deliana Escobari <delianaescobari@gmail.com>
2017-01-06 12:04:08 -0600

    Create data items to go on table in Dynamo DB

--- samples/src/main/java/dynamo/CoffeeCupDynamoDbClient.java
+++ samples/src/main/java/dynamo/CoffeeCupDynamoDbClient.java
@@ -1,7 +1,7 @@
 package dynamo;
 
-import com.amazon.speech.speechlet.Session;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 
 /**
  * Methods to interact with the persistence layer for CoffeeCup in DynamoDB
@@ -15,5 +15,24 @@ public class CoffeeCupDynamoDbClient {
 		this.dynamoDBClient = dynamoDBClient;
 	}
 	
-
+	/**
+	 * Loads item from DynamoDB by the primary hash key. 
+	 * @param tableItem
+	 * @return
+	 */
+	public CoffeeCupDataItem loadItem(final CoffeeCupDataItem tableItem){
+		DynamoDBMapper mapper = createDynamoDBMapper();
+		CoffeeCupDataItem item = mapper.load(tableItem);
+		
+		return item;
+	}
+	
+	public void saveItem(final CoffeeCupDataItem tableItem){
+		DynamoDBMapper mapper = createDynamoDBMapper();
+		mapper.save(tableItem);
+	}
+	
+	private DynamoDBMapper createDynamoDBMapper(){
+		return new DynamoDBMapper(dynamoDBClient);
+	}
 }
